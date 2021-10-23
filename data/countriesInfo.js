a613d2d1447ff22286ccecb6484f2343a94bb9e1
Bartosz Kowal <bartoszkowal96@gmail.com>
2021-10-23 12:09:55 +0200

    deploy covid-traveller to github

---
+++ data/countriesInfo.js
@@ -0,0 +1,708 @@
+const countriesInfo = {
+    "data": [
+        {
+            "country": "Albania",
+            "others": [
+            {
+                "code": "Greece", "color": "green"
+            },
+            {
+                "code": "Macedonia", "color": "green"
+            },
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Kosovo", "color": "green"
+            }            
+            ]
+        },
+
+        {
+            "country": "Austria",
+            "others": [
+            {
+                "code": "Switzerland", "color": "green"
+            },
+            {
+                "code": "Italy", "color": "green"
+            },
+            {
+                "code": "Slovenia", "color": "yellow"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            },
+            {
+                "code": "Slovakia", "color": "green"
+            },
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Czechia", "color": "red"
+            }          
+            ]
+        },
+
+        {
+            "country": "Belgium",
+            "others": [
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Luxembourg", "color": "orange"
+            },
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Netherlands", "color": "green"
+            }      
+            ]
+        },
+
+        {
+            "country": "Bulgaria",
+            "others": [
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Macedonia", "color": "green"
+            },
+            {
+                "code": "Greece", "color": "green"
+            },
+            {
+                "code": "Romania", "color": "red"
+            }        
+            ]
+        },
+
+        {
+            "country": "Bosnia and Herzegovina",
+            "others": [
+            {
+                "code": "Croatia", "color": "orange"
+            },
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            } 
+            ]
+        },
+
+        {
+            "country": "Belarus",
+            "others": [
+            {
+                "code": "Latvia", "color": "green"
+            },
+            {
+                "code": "Lithuania", "color": "green"
+            },
+            {
+                "code": "Poland", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            },
+            {
+                "code": "Ukraine", "color": "red"
+            }        
+            ]
+        },
+
+        {
+            "country": "Switzerland",
+            "others": [
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Italy", "color": "green"
+            }      
+            ]
+        },
+
+        {
+            "country": "Czechia",
+            "others": [
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "Slovakia", "color": "green"
+            },
+            {
+                "code": "Poland", "color": "green"
+            }      
+            ]
+        },
+
+        {
+            "country": "Denmark",
+            "others": [
+            {
+                "code": "Germany", "color": "red"
+            }     
+            ]
+        },
+
+        {
+            "country": "Germany",
+            "others": [
+            {
+                "code": "Poland", "color": "green"
+            },
+            {
+                "code": "Denmark", "color": "red"
+            },
+            {
+                "code": "Czechia", "color": "red"
+            },
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "Switzerland", "color": "green"
+            },
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Luxembourg", "color": "orange"
+            },
+            {
+                "code": "Belgium", "color": "yellow"
+            },
+            {
+                "code": "Netherlands", "color": "green"
+            }  
+            ]
+        },
+
+        {
+            "country": "Spain",
+            "others": [
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Portugal", "color": "green"
+            }      
+            ]
+        },
+
+        {
+            "country": "Estonia",
+            "others": [
+            {
+                "code": "Latvia", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            }      
+            ]
+        },
+
+        {
+            "country": "Finland",
+            "others": [
+            {
+                "code": "Norway", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            },
+            {
+                "code": "Sweden", "color": "red"
+            }        
+            ]
+        },
+
+        {
+            "country": "France",
+            "others": [
+            {
+                "code": "Belgium", "color": "yellow"
+            },
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Spain", "color": "orange"
+            },
+            {
+                "code": "Italy", "color": "green"
+            },
+            {
+                "code": "Switzerland", "color": "green"
+            },
+            {
+                "code": "Luxembourg", "color": "orange"
+            }          
+            ]
+        },
+
+        {
+            "country": "United Kingdom",
+            "others": [
+            {
+                "code": "Ireland", "color": "green"
+            }  
+            ]
+        },
+
+        {
+            "country": "Greece",
+            "others": [
+            {
+                "code": "Albania", "color": "yellow"
+            },
+            {
+                "code": "Bulgaria", "color": "red"
+            },
+            {
+                "code": "Macedonia", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Croatia",
+            "others": [
+            {
+                "code": "Bosnia and Herzegovina", "color": "green"
+            },
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Slovenia", "color": "yellow"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Hungary",
+            "others": [
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "Croatia", "color": "orange"
+            },
+            {
+                "code": "Romania", "color": "red"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Slovakia", "color": "green"
+            },
+            {
+                "code": "Slovenia", "color": "yellow"
+            },
+            {
+                "code": "Ukraine", "color": "red"
+            }
+            ]
+        },
+
+        {
+            "country": "Ireland",
+            "others": [
+            {
+                "code": "United Kingdom", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Iceland",
+            "others": [
+            {}
+            ]
+        },
+
+        {
+            "country": "Italy",
+            "others": [
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Slovenia", "color": "yellow"
+            },
+            {
+                "code": "Switzerland", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Kosovo",
+            "others": [
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Albania", "color": "yellow"
+            },
+            {
+                "code": "Macedonia", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Lithuania",
+            "others": [
+            {
+                "code": "Belarus", "color": "orange"
+            },
+            {
+                "code": "Latvia", "color": "green"
+            },
+            {
+                "code": "Poland", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Luxembourg",
+            "others": [
+            {
+                "code": "Belgium", "color": "yellow"
+            },
+            {
+                "code": "France", "color": "orange"
+            },
+            {
+                "code": "Germany", "color": "red"
+            }
+            ]
+        },
+
+        {
+            "country": "Latvia",
+            "others": [
+            {
+                "code": "Lithuania", "color": "green"
+            },
+            {
+                "code": "Belarus", "color": "orange"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            },
+            {
+                "code": "Estonia", "color": "red"
+            }
+            ]
+        },
+
+        {
+            "country": "Moldova",
+            "others": [
+            {
+                "code": "Ukraine", "color": "red"
+            },
+            {
+                "code": "Romania", "color": "red"
+            }
+            ]
+        },
+
+        {
+            "country": "Macedonia",
+            "others": [
+            {
+                "code": "Albania", "color": "yellow"
+            },
+            {
+                "code": "Bulgaria", "color": "red"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Kosovo", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Montenegro",
+            "others": [
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Kosovo", "color": "green"
+            },
+            {
+                "code": "Albania", "color": "yellow"
+            }
+            ]
+        },
+
+        {
+            "country": "Netherlands",
+            "others": [
+            {
+                "code": "Belgium", "color": "yellow"
+            },
+            {
+                "code": "Germany", "color": "red"
+            }
+            ]
+        },
+
+        {
+            "country": "Norway",
+            "others": [
+            {
+                "code": "Finland", "color": "green"
+            },
+            {
+                "code": "Sweden", "color": "red"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Poland",
+            "others": [
+            {
+                "code": "Germany", "color": "red"
+            },
+            {
+                "code": "Czechia", "color": "red"
+            },
+            {
+                "code": "Slovakia", "color": "green"
+            },
+            {
+                "code": "Ukraine", "color": "red"
+            },
+            {
+                "code": "Belarus", "color": "orange"
+            },
+            {
+                "code": "Lithuania", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Portugal",
+            "others": [
+            {
+                "code": "Spain", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Romania",
+            "others": [
+            {
+                "code": "Bulgaria", "color": "red"
+            },
+            {
+                "code": "Moldova", "color": "yellow"
+            },
+            {
+                "code": "Serbia", "color": "red"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            },
+            {
+                "code": "Belarus", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Russia",
+            "others": [
+            {
+                "code": "Norway", "color": "green"
+            },
+            {
+                "code": "Finland", "color": "green"
+            },
+            {
+                "code": "Estonia", "color": "red"
+            },
+            {
+                "code": "Latvia", "color": "green"
+            },
+            {
+                "code": "Belarus", "color": "orange"
+            },
+            {
+                "code": "Ukraine", "color": "red"
+            },
+            {
+                "code": "Lithuania", "color": "green"
+            },
+            {
+                "code": "Poland", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Serbia",
+            "others": [
+            {
+                "code": "Hungary", "color": "orange"
+            },
+            {
+                "code": "Romania", "color": "red"
+            },
+            {
+                "code": "Bulgaria", "color": "red"
+            },
+            {
+                "code": "Macedonia", "color": "green"
+            },
+            {
+                "code": "Albania", "color": "yellow"
+            },
+            {
+                "code": "Montenegro", "color": "orange"
+            },
+            {
+                "code": "Bosnia and Herzegovina", "color": "green"
+            },
+            {
+                "code": "Croatia", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Slovakia",
+            "others": [
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "Czechia", "color": "red"
+            },
+            {
+                "code": "Poland", "color": "green"
+            },
+            {
+                "code": "Ukraine", "color": "red"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            }
+            ]
+        },
+
+        {
+            "country": "Sweden",
+            "others": [
+            {
+                "code": "Finland", "color": "green"
+            },
+            {
+                "code": "Norway", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Slovenia",
+            "others": [
+            {
+                "code": "Austria", "color": "red"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            },
+            {
+                "code": "Croatia", "color": "orange"
+            },
+            {
+                "code": "Italy", "color": "green"
+            }
+            ]
+        },
+
+        {
+            "country": "Ukraine",
+            "others": [
+            {
+                "code": "Belarus", "color": "orange"
+            },
+            {
+                "code": "Moldova", "color": "yellow"
+            },
+            {
+                "code": "Poland", "color": "green"
+            },
+            {
+                "code": "Russia", "color": "orange"
+            },
+            {
+                "code": "Romania", "color": "red"
+            },
+            {
+                "code": "Slovakia", "color": "green"
+            },
+            {
+                "code": "Hungary", "color": "orange"
+            }
+            ]
+        }
+    ] 
+};
+
+export default countriesInfo;
---
+++ script.js
@@ -0,0 +1,96 @@
+import countriesBorders from "./data/countriesBorders";
+import countriesCodes from "./data/countriesCodes";
+import countriesInfo from "./data/countriesInfo";
+
+//COUNTRY HIGHLIGHTNING SETTINGS
+const GEO_COUNTRY_WEIGHT = 3;
+const GEO_COUNTRY_OPACITY = 0.5;
+
+//MAP DISPLAY SETTINGS
+const GE0_MAP_COLOR = "lightgreen";
+const GE0_MAP_WEIGHT = 4;
+const GE0_MAP_OPACITY = 0.5;
+const GEO_MAP_CENTER = [52.23, 21.22];
+const GEO_MAP_ZOOM = 5;
+const GEO_MAP_MIN_ZOOM = 5;
+
+//CREATE A WORLD MAP
+const myMap = L.map('mapid', {
+    minZoom: GEO_MAP_MIN_ZOOM
+}).setView(GEO_MAP_CENTER, GEO_MAP_ZOOM);
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(myMap);
+
+const mapStyle = {
+    "color": GE0_MAP_COLOR,
+    "weight": GE0_MAP_WEIGHT,
+    "opacity": GE0_MAP_OPACITY
+};
+
+const mapBorders = L.geoJSON(countriesBorders, {style: mapStyle});
+
+const mapLayers = L.layerGroup([mapBorders]);
+mapLayers.addTo(myMap);
+
+//HANDLE CLICK ON A COUNTRY
+const handleClick = (event) => {
+    //READ CO-ORDINATES
+    const lat = Math.round(event.latlng.lat*100)/100;
+    const lng = Math.round(event.latlng.lng*100)/100;
+
+    //"GEONAMES API" REQUEST - GET COUNTRY ISO CODE
+    const URL = `http://api.geonames.org/countryCode?lat=${lat}&lng=${lng}&username=barkow96`;
+    const http = new XMLHttpRequest();
+    http.open("GET", URL);
+    http.send();
+    
+    http.onreadystatechange = (e) => {
+        const countryCode = http.responseText.slice(0,2);
+        const country = decipherCountry(countryCode);
+
+        const geoStyle = {
+            "color": "",
+            "weight": GEO_COUNTRY_WEIGHT,
+            "opacity": GEO_COUNTRY_OPACITY
+        };
+        const geoCountry = {
+            "type": "FeatureCollection",
+            "features": []
+        };
+
+        let marker;
+        for (const record of countriesInfo.data) {
+            if (record.country === country) {
+                const others = record.others;
+                for (const other of others) {
+                    for (const land of countriesBorders.features) {
+                        if (land.properties.name === other.code) {
+                            geoCountry.features[0] = land;
+                            geoStyle.color = other.color;
+
+                            marker = L.geoJSON(geoCountry, {style: geoStyle});
+                            mapLayers.addLayer(marker);
+                        };
+                    };
+                };
+                break;
+            };
+        
+        mapLayers.clearLayers();
+        mapLayers.addLayer(mapBorders);
+        mapLayers.addTo(myMap);
+    };
+    };
+};
+
+//FUNCTION THAT RETURNS COUNTRY NAME ON THE BASIS OF ITS ISO CODE
+const decipherCountry = (countryCode) => {
+    for (const code in countriesCodes) if (countryCode == code) return countriesCodes[code];
+};
+
+//EVENT LISTENER FOR CLICKING ON THE MAP
+myMap.addEventListener("click", (event) => handleClick(event));
